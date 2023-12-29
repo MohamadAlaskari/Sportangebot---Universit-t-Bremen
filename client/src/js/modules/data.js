ba4b07e5a6a45cb4e00ab8cdd6aed9898e5370c2
MohamadAlaskari <m_alskari96@hotmail.com>
2023-12-29 23:45:12 +0100

    use json as raw link

--- client/src/js/modules/data.js
+++ client/src/js/modules/data.js
@@ -1,18 +1,17 @@
 // Function to asynchronously load course data
 async function loadKurseData() {
-    try {
-      const response = await fetch("../../data/kurseData.json");  
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      return data.kurse; // Assuming the data is stored under the key "kurse"
-    } catch (e) {
-      console.error("Error loading course data:", e);
-      return []; // Return an empty array in case of error
+  try {
+    const response = await fetch("https://github.com/MohamadAlaskari/Sportangebot---Universit-t-Bremen/blob/ModuleJs/client/src/data/kurseData.json");
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+    const data = await response.json();
+    return data.kurse; // Assuming the data is stored under the key "kurse"
+  } catch (e) {
+    console.error("Error loading course data:", e);
+    return []; // Return an empty array in case of error
   }
-  
-  // Export the function to use it in other modules
-  export { loadKurseData };
-  
+}
+
+// Export the function to use it in other modules
+export { loadKurseData };
